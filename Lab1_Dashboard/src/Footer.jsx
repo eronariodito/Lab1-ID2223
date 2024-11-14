50947bd269e5d93e6c5c4aa57df668f051941234
eronariodito <67965868+eronariodito@users.noreply.github.com>
2024-11-14 22:12:46 +0100

    Added react project

---
+++ Lab1_Dashboard/src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <>
+      {" "}
+      <p>Creted for Lab 1 ID2333 Scalable Machine Learning</p>
+      <p>Eron Ariodito (ariodito@kth.se)</p>
+      <p>Eron Ariodito (ariodito@kth.se)</p>
+    </>
+  );
+}
---
+++ Lab1_Dashboard/src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <div className="w-screen bg-black h-min">
+        <p className="text-center bg-black">
+          PM2.5 Prediction for island of Java
+        </p>
+      </div>
+    </>
+  );
+}
