511e257be3aaae0e9dc3cdbc25a1d966bc478247
eronariodito <67965868+eronariodito@users.noreply.github.com>
2024-11-20 04:24:05 +0100

    asdfawef

--- Lab1_Dashboard/vite.config.js
+++ Lab1_Dashboard/vite.config.js
@@ -11,7 +11,12 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ["chart.js", "react-chartjs-2", "chartjs-adapter-date-fns"], // Externalize chart.js to prevent bundling
+      external: [
+        "chart.js",
+        "react-chartjs-2",
+        "chartjs-adapter-date-fns",
+        "date-fns",
+      ], // Externalize chart.js to prevent bundling
     },
   },
 });
